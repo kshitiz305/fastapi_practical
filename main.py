1cc8af18c09c048b6a348c5be1c7ee6267c819fa
Kshitiz <kshitiz305@live.com>
2022-12-05 18:21:49 +0530

    Initial file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 
@@ -9,13 +11,16 @@ BOOKS = {1:"FIRST",2:"SECOND"
          }
 
 
-@app.get('/')
-async def get_book():
-    return "Hello User"
+@app.get('/assignment')
+async def read_book(bookname:Optional[int]= None):
+    if bookname:
+        return BOOKS[bookname]
+    return BOOKS
 
-@app.get('/{bookname}')
-async def read_book(bookname:int):
-    return BOOKS[bookname]
+
+# @app.get('/')
+# async def get_book():
+#     return "Hello User"
 
 
 
